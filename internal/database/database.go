4ea21932418cacc977291d64fbfb2023a9675a1d
EgrShishov <e.shishov98@yandex.ru>
2023-07-05 15:43:27 +0300

    added connect func

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,5 +1,36 @@
 package database
 
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type Idatabase interface {
+}
+
+type database struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DbName   string
+}
+
+func New(cfg database) (*sqlx.DB, error) {
+	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s",
+		cfg.Host, cfg.Port, cfg.Username, cfg.DbName, cfg.Password))
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Successfully connected to DB")
+	return db, nil
+}
+
 /*
  * TODO:
  * init database struct
