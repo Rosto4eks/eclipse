eedea48773f3f1d73a1beb665f2bdc8db205f82b
EgrShishov <e.shishov98@yandex.ru>
2023-07-05 22:58:11 +0300

    minor changes

--- internal/models/articles.go
+++ internal/models/articles.go
@@ -1,13 +1,11 @@
 package models
 
-import "time"
-
 type Articles struct {
 	ID          int
 	Name        string
 	Theme       string
 	AuthorID    int
 	ImagesCount int
-	Date        time.Time
+	Date        string
 	Text        string
 }
