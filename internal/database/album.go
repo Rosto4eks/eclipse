b7c89813006669869fc447847741e6e060b79ba7
EgrShishov <e.shishov98@yandex.ru>
2023-07-05 22:56:56 +0300

    change funcs implementation

--- internal/database/album.go
+++ internal/database/album.go
@@ -1,52 +1,44 @@
 package database
 
 import (
-	"fmt"
 	"github.com/Rosto4eks/eclipse/internal/models"
 )
 
 func (d *database) GetAllAlbums() ([]models.Album, error) {
-	query := fmt.Sprintf("SELECT * FROM albums;")
-	rows, err := d.db.Query(query)
-	if err != nil {
+	query := "SELECT * FROM albums;"
+	var response []models.Album
+	if err := d.db.Get(&response, query); err != nil {
 		return nil, err
 	}
 
-	var response []models.Album
-	if err = rows.Scan(&response); err != nil {
-		return nil, err
-	}
 	return response, nil
 }
 
 func (d *database) GetAlbumByID(ID int) (models.Album, error) {
-	query := fmt.Sprintf("SELECT name, author_id, images_count, date, description FROM albums WHERE id = $1;", ID)
-	row := d.db.QueryRow(query)
-
+	query := "SELECT * FROM albums WHERE id = $1;"
 	var response models.Album
-	if err := row.Scan(&response); err != nil {
+
+	if err := d.db.Select(&response, query, ID); err != nil {
 		return models.Album{}, err
 	}
+
 	return response, nil
 }
 
 func (d *database) AddAlbum(album models.Album) error {
-	query := fmt.Sprintf("INSERT INTO albums (name, author_id, images_count, date, description) VALUES($1,$2,$3,$4,$5);",
-		album.Name, album.Author, album.Count, album.Date, album.Description)
-	result, err := d.db.Exec(query)
-	if err != nil {
-		return nil
+	query := "INSERT INTO albums (name, author_id, images_count, date, description) VALUES($1,$2,$3,$4,$5) RETURNING id;"
+	if _, err := d.db.Exec(query, album.Name, album.Author, album.Count, album.Date, album.Description); err != nil {
+		return err
 	}
-	fmt.Print(result)
+
 	return nil
 }
 
 func (d *database) DelAlbumByID(ID int) error {
-	query := fmt.Sprintf("DELETE FROM albums WHERE id = $1;", ID)
-	result, err := d.db.Exec(query)
-	if err != nil {
+	query := "DELETE FROM albums WHERE id = $1;"
+	if _, err := d.db.Exec(query, ID); err != nil {
 		return err
 	}
-	fmt.Print(result)
+
 	return nil
 }
