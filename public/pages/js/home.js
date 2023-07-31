1c7f8dbc1640682656252d638e045f18bd0c8a39
EgrShishov <e.shishov98@yandex.ru>
2023-07-31 17:29:22 +0300

    minor changes, minor redesign

--- public/pages/js/home.js
+++ public/pages/js/home.js
@@ -15,24 +15,3 @@ function changeGif(){
 }
 
 setInterval(changeGif, 2500);
-
-window.addEventListener(`scroll`,trackScroll);
-let element = document.getElementById(`container_page`);
-let another = document.getElementById(`introduction`);
-let inIntroduction = true;
-
-function trackScroll(){
-    const YPos = another.scrollTop;
-    console.log(this.scrollY, inIntroduction);
-    const height = document.documentElement.clientHeight;
-    // if(this.scrollY < 700 && !inIntroduction){
-    //     another.scrollIntoView({behavior: "smooth"});
-    //     inIntroduction = true;
-    //     return;
-    // }
-
-    // if(this.scrollY > 125 && this.scrollY < 350 && inIntroduction){
-    //     element.scrollIntoView({behavior : "smooth"});
-    //     inIntroduction = false;
-    // }
-}
